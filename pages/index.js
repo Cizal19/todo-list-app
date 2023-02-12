49e6935674746020bb5f03b40212df0f96f8a848
Cizzeros <gautamcizal@gmail.com>
2023-02-12 16:24:40 +0545

    implemented drag and drop functionality
    implemented drag and drop from same column to same column as well as from one column to another

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,88 @@
 import { Flex, Heading, Text } from "@chakra-ui/react"
+import dynamic from "next/dynamic"
 import React, { useState } from "react"
-import Column from "../src/Column"
 import { DragDropContext } from "react-beautiful-dnd"
 import { initialData } from "../src/data"
+
+const Column = dynamic(() => import("../src/Column"), {ssr: false})
+
+const reorderColumnList = (sourceCol, startIndex, endIndex) => {
+  const newTaskIds = Array.from(sourceCol.taskIds);
+  const [removed] = newTaskIds.splice(startIndex, 1);
+  newTaskIds.splice(endIndex, 0, removed);
+
+  const newColumn = {
+    ...sourceCol,
+    taskIds: newTaskIds,
+  };
+
+  return newColumn;
+};
 
 export default function Home() {
   const [state, setState] = useState(initialData)
 
   const handleDragEnd = (result) => {
-    const {destination, source} = result
+    const { destination, source } = result;
+
+    // If user tries to drop in an unknown destination
+    if (!destination) return;
+
+    // if the user drags and drops back in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // If the user drops within the same column but in a different positoin
+    const sourceCol = state.columns[source.droppableId];
+    const destinationCol = state.columns[destination.droppableId];
+
+    if (sourceCol.id === destinationCol.id) {
+      const newColumn = reorderColumnList(
+        sourceCol,
+        source.index,
+        destination.index
+      );
+
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      setState(newState);
+      return;
+    }
+
+    // If the user moves from one column to another
+    const startTaskIds = Array.from(sourceCol.taskIds);
+    const [removed] = startTaskIds.splice(source.index, 1);
+    const newStartCol = {
+      ...sourceCol,
+      taskIds: startTaskIds,
+    };
+
+    const endTaskIds = Array.from(destinationCol.taskIds);
+    endTaskIds.splice(destination.index, 0, removed);
+    const newEndCol = {
+      ...destinationCol,
+      taskIds: endTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol,
+      },
+    };
+
+    setState(newState);
   }
 
   return (
--- src/Column.js
+++ src/Column.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { Flex, Text } from "@chakra-ui/react"
-import { Droppable } from "react-beautiful-dnd";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 const Column = ({column, tasks}) => {
   return(
@@ -17,15 +17,34 @@ const Column = ({column, tasks}) => {
           {column.title}
         </Text>
       </Flex>
-
-      <Droppable>
-        <Flex px="1.5rem" flex={1} flexDir="column">
-          {tasks.map(task => (
-            <Flex mb="1rem" h="72px" bg="card-bg" rounded="3px" p="1.5rem">
-              <Text>{task.content}</Text>
-            </Flex>
-          ))}
-        </Flex>
+      <Droppable droppableId={column.id}>
+        {(droppableProvided, droppableSnapshot) => (
+          <Flex 
+            px="1.5rem" flex={1} 
+            flexDir="column" 
+            ref={droppableProvided.innerRef} 
+            {...droppableProvided.droppableProps}
+          >
+            {tasks.map((task, index) => (
+              <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
+                {(draggableProvided, draggableSnapshot) => (
+                  <Flex 
+                    mb="1rem" 
+                    h="72px" 
+                    bg="card-bg" 
+                    rounded="3px" 
+                    p="1.5rem"
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.draggableProps}
+                    {...draggableProvided.dragHandleProps}
+                  >
+                    <Text>{task.content}</Text>
+                  </Flex>
+                )}
+              </Draggable>
+            ))}
+          </Flex>
+        )}
       </Droppable>
     </Flex>
   )
